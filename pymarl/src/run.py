431c8cb2a0592a3070f8e647e4de3e947ee77d75
lych1233 <lych1233@gmail.com>
2022-05-17 12:21:53 +0800

    add noise in run.py

--- pymarl/src/run.py
+++ pymarl/src/run.py
@@ -106,6 +106,7 @@ def save_one_buffer(args, save_buffer, env_name, from_start=False):
 
 
 def run_sequential(args, logger):
+    th.set_num_threads(8)
 
     # Init runner so we can get env info
     runner = r_REGISTRY[args.runner](args=args, logger=logger)
